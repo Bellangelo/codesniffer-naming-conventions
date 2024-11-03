d2d49a89458d86c819319d833fb4a1eaa7cf86a2
Bellangelo <aggelosbellos7@gmail.com>
2024-11-03 17:39:35 +0200

    Construct the idea for the singular sniff

--- src/Sniffs/SingularClassNameSniff.php
+++ src/Sniffs/SingularClassNameSniff.php
@@ -1,6 +1,35 @@
 <?php
 
-class SingularClassNameSniff
-{
+declare(strict_types=1);
 
+namespace Bellangelo\CodesnifferNamingConventions\Sniffs;
+
+use Override;
+use PHP_CodeSniffer\Files\File;
+use PHP_CodeSniffer\Sniffs\Sniff;
+
+final class SingularClassNameSniff implements Sniff
+{
+    #[Override] public function register()
+    {
+        return [
+            T_CLASS,
+        ];
+    }
+
+    #[Override] public function process(File $phpcsFile, $stackPtr)
+    {
+        $classNamePointer = $phpcsFile->findNext([T_STRING], $stackPtr);
+
+        if ($classNamePointer === false) {
+            return;
+        }
+
+        $tokens = $phpcsFile->getTokens();
+        $className = $tokens[$classNamePointer]['content'];
+
+        if (substr($className, -1) === 's') {
+            $phpcsFile->addError('Class name should be singular', $stackPtr);
+        }
+    }
 }
