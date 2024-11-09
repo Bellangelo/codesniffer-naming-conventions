d24bfbda919c39a94bfd1ec4b538f839d362b010
Bellangelo <aggelosbellos7@gmail.com>
2024-11-09 04:05:06 +0200

    Fix code style

--- rector.php
+++ rector.php
@@ -1,20 +1,11 @@
-<?php
-
-declare(strict_types=1);
+<?php declare(strict_types = 1);
 
 use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
 	->withPaths([__DIR__ . '/bin', __DIR__ . '/src', __DIR__ . '/tests'])
 	->withSkip([__DIR__ . '/tests/Commands/data'])
-	->withPreparedSets(
-		codeQuality: true,
-		codingStyle: true,
-		typeDeclarations: true,
-		privatization: true,
-		naming: true,
-		earlyReturn: true
-	)
+	->withPreparedSets(codeQuality: true, codingStyle: true, typeDeclarations: true, privatization: true, naming: true, earlyReturn: true)
 	->withPhpSets()
 	->withRootFiles()
 	->withImportNames();
