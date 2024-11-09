72a580f08950b958878f3ddaf69b5c0009294772
Bellangelo <aggelosbellos7@gmail.com>
2024-11-09 02:50:37 +0200

    Codestyle issues

--- tests/Sniffs/data/PluralInClassNames.fixed.php
+++ tests/Sniffs/data/PluralInClassNames.fixed.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Bellangelo\CodesnifferNamingConventions\Tests\Sniffs\data;
+namespace Bellangelo\CodesnifferNamingConventions\Sniffs\data;
 
 class PluralInClassName
 {
